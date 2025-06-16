ff8c191ee68933e62c9ee913f63ce53c70ba1b7d
marcosamaral2000 <mrcsvncsamaral@gmail.com>
2025-06-16 15:20:18 -0300

    Configurando bariáveis de ambiente

--- backend-tarefas/db.js
+++ backend-tarefas/db.js
@@ -1,9 +1,14 @@
 import mysql from "mysql2/promise"
+import { configDotenv } from "dotenv";
+
+configDotenv()
 
 const connection = await mysql.createConnection({
-host: "localhost",
-user: "root",
-password: "root",
-database: "tarefas_db"
+host: process.env.DB_HOST,
+port: process.env.DB_PORT,
+user: process.env.DB_USER,
+password: process.env.DB_PASSWORD,
+database: process.env.DB_NAME
 })
+
 export default connection;
